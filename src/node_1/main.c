74cc952752ffdc3fa159a503daef747315ca12ed
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-09-09 15:18:49 +0200

    Add code to test uart library to main file

---
+++ src/node_1/main.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include "lib/uart_com.h"
+
+#define F_CPU 4915200 //Clock Speed
+#include <util/delay.h>
+
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
+
+void main()
+{
+		uart_init(MYUBRR);
+		unsigned char letter = 0x41;
+		while (1)
+		{
+				letter = uart_read();
+				uart_transmit(letter);
+				_delay_ms(10);
+		}
+}
