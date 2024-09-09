4742013a6100f2c4684f8ce7179824e59f975b29
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-09-09 10:59:47 +0200

    Functional main for receiving and re-transmitting chars over uart

--- main.c
+++ main.c
@@ -2,17 +2,22 @@
 #include <avr/io.h>
 #include "uart_driver.h"
 
-#define FOSC 4915200 //Clock Speed
+
+#define F_CPU 4915200 //Clock Speed
+#include <util/delay.h>
+
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR F_CPU/16/BAUD-1
 
 void main()
 {
 		uart_init(MYUBRR);
-		unsigned char letter = '0';
+		unsigned char letter = 0x41;
+		_delay_ms(10);
 		while (1)
 		{
-				//letter = uart_receive();
+				letter = uart_receive();
 				uart_transmit(letter);
+				_delay_ms(100);
 		}
 }
