88870273c4228ce266e8b26ef67f178eca0e0ac1
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-09-09 15:06:21 +0200

    Update uart_com.c and uart_com.h to align with gcc recommendations and code standard.
    Change to ifndef in place of pragma once, as gcc notes that pragma once
    is outdated and should not be used.
    
    Change function names to be lower case, as per Barr C coding standard.
    
    Add some documentation.

--- src/node_1/lib/uart_com.c
+++ src/node_1/lib/uart_com.c
@@ -1,30 +1,45 @@
+//NOTE: Code is heavily inspired by code found in the Atmega162 datasheet.
+#include <avr/io.h>
+#include <avr/common.h>
 #include "uart_com.h"
 
-void UART_INIT(unsigned int ubrr){
-	UBRR0H = (unsigned char)(ubrr >> 8);
-	
-	UBRR0L = (unsigned char)ubrr;
-	
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-	
-	UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);
-	
-	fdevopen(UART_TRANSMIT,UART_READ);
+//Initialises UART communication.
+//Takes in a given UBRR value. (UART Baud Rate Register)
+void uart_init(unsigned int ubbr)
+{
+		//Set baud rate:
+		UBRR0H = (unsigned char) (ubbr >> 8);
+		UBRR0L = (unsigned char) ubbr;
+
+		//Enable receiver and transmitter:
+		UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+
+		//Set frame format: 8data, 2stop bit:
+		UCSR0C = (1<<URSEL0) | (1<<USBS0) | (3<<UCSZ00);
+		fdevopen(uart_transmit,uart_read);
 }
 
-void UART_TRANSMIT(unsigned char c){
-	while (!(UCSR0A & (1<<UDRE0))){
-		;
-	}
-	/* Put data into buffer, sends the data */
-	UDR0 = c;
+//Transmits one character over UART
+//Takes in the character to transmit
+void uart_transmit(unsigned char data)
+{
+		// Wait for empty transmit buffer
+		while ( !( UCSR0A & (1<<UDRE0)) )
+		{
+				; //Do nothing
+		}
+		// Put data into buffer, sends the data
+		UDR0 = data;
 }
 
-uint8_t UART_READ(void){
-	/* Wait for data to be received */
-	while ( !(UCSR0A & (1<<RXC0)) ){
-		;
-	}
-	/* Get and return received data from buffer */
-	return UDR0;
+//Recieves one character from UART
+unsigned char uart_read(void)
+{
+		// Wait for data to be received
+		while ( !(UCSR0A & (1<<RXC0)) )
+		{
+				; //Do nothing
+		}
+		// Get and return received data from buffer
+		return UDR0;
 }
