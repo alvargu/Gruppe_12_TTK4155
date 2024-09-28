a9d97d466673bc1c8ad28ffab81bb4169b632203
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-09-28 12:43:31 +0200

    Add include guard to joystick.h

--- src/node_1/lib/joystick.h
+++ src/node_1/lib/joystick.h
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include "ADC_Driver.h"
 
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
 typedef struct //Joystick angle in x and y direction, with the angle as a percentage (from -100 to 0, from 0 to +100).
 {
 		//Note that these are signed values
@@ -28,3 +31,4 @@ void joystick_get_angle(joystick_angle_t*, raw_adc_data_t*);
 //Gets the direction of the joystick. 
 joystick_direction joystick_get_direction(raw_adc_data_t*);
 
+#endif
