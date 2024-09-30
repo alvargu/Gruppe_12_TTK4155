0573d25e165fb3ae47d2e8ba67b9bd82e9ae3764
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-09-30 17:56:37 +0200

    Update adc_driver to use xmem.h

--- src/node_1/lib/ADC_Driver.c
+++ src/node_1/lib/ADC_Driver.c
@@ -4,7 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ADC_Driver.h"
+#include "xmem.h"
 
+static uint8_t adc_read(void)
+{
+		return xmem_read(XMEM_ADC, 0u); //It never matters which
+										//adress in the ADC we 
+										//read from, so we just 
+										//read from adress 0
+}
+
+static void adc_write(void) //We never write a meaningful value to
+							//the adc, so the function takes no
+							//parameters
+{
+		xmem_write(XMEM_ADC, 0u, 0u); //It never matters which
+}                                     //adress in the ADC we 
+                                      //read from, so we just 
+                                      //read from adress 0
 
 //Initialises PD5 as an output pin for PWM, and sets up Counter 3 to
 //produce a PWM signal.
@@ -21,18 +38,15 @@ void adc_init()
 //Prints all input values from ADC (should be placed in a loop)
 void adc_test()
 {
-		volatile char *adc_start_address = (char *) 0x1400; //0x1400 is the first adress in the adress space.
 		raw_adc_data_t adc_inputs;
+		adc_write();
 
-		adc_start_address[0] = 0x00; //Write any value just to send write signal to ADC.
-
-	    //For read and write, the adress does not matter as long as it is within the ADC adress space
 		//The order of reads matters here, as the ADC inputs are stored in the ADC's RAM in a certain order (Pin 0 -> Pin 3).
-		//Reading "pops" values from the RAM in FIFO order.
-		adc_inputs.joystick_x = adc_start_address[0];
-		adc_inputs.joystick_y = adc_start_address[0];
-		adc_inputs.slider_left = adc_start_address[0];
-		adc_inputs.slider_right = adc_start_address[0];
+		//Reading "pops" values from the ADC's RAM in FIFO order.
+		adc_inputs.joystick_x = adc_read();
+		adc_inputs.joystick_y = adc_read();
+		adc_inputs.slider_left = adc_read();
+		adc_inputs.slider_right = adc_read();
 
 		printf("\r");
 		printf("x : %8d | y: %8d || left: %8d | right: %8d", adc_inputs.joystick_x, adc_inputs.joystick_y, adc_inputs.slider_right, adc_inputs.slider_left);
@@ -41,15 +55,13 @@ void adc_test()
 //Samples current values from the ADC
 void adc_sample(raw_adc_data_t* adc_inputs) 
 {
-		volatile char *adc_start_address = (char *) 0x1400; //0x1400 is the first adress in the adress space.
-		adc_start_address[0] = 0x00; //Write any value just to send write signal to ADC.
-
+		adc_write();
 	    //For read and write, the adress does not matter as long as it is within the ADC adress space
 		//The order of reads matters here, as the ADC inputs are stored in the ADC's RAM in a certain order (Pin 0 -> Pin 3).
 		//Reading "pops" values from the RAM in FIFO order.
-		adc_inputs -> joystick_x = adc_start_address[0];
-		adc_inputs -> joystick_y = adc_start_address[0];
-		adc_inputs -> slider_left = adc_start_address[0];
-		adc_inputs -> slider_right = adc_start_address[0];
+		adc_inputs -> joystick_x = adc_read();
+		adc_inputs -> joystick_y = adc_read();
+		adc_inputs -> slider_left = adc_read();
+		adc_inputs -> slider_right = adc_read();
 }
 
