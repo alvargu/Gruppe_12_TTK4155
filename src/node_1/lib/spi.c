c471d87f7c3d49605d2a495d39b90ba42261f935
Michalstank <michal2stan@gmail.com>
2024-10-14 15:49:56 +0200

    SPI master and slave inits + rx & tx functions

--- src/node_1/lib/spi.c
+++ src/node_1/lib/spi.c
@@ -1,23 +1,47 @@
 #include "spi.h"
 
 void
-spi_init()
+spi_master_init()
 {
-    //TODO:
-    //SPI Registers config
-    DDR_SPI = (1 << DD_MOSI) | (1 << DD_SCK);
+	//SPI IO Registers config
+	DDRB = (1 << DDB5) | (1 << DDB7);
 
-    //Enable and configure SPI as master with clock rate f_clk/16
-    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+	//Enable and configure SPI as master with clock rate f_clk/16
+	//Additionaly configures SPI to function in Mode 1,1
+    //! Consider if it is supposed to function with an interrupt
+	SPCR = /*(1 << SPIE) |*/ (1 << SPE) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA)| (1 << SPR0);
+}
+
+void
+spi_slave_init()
+{
+	//SPI Direction Register Config
+	//Setup MISO as output everything else input
+	DDRB = (1 << DDB6);
+	
+	//Enable SPI
+	SPCR = (1 << SPE);
 }
 
 void
 spi_transmit(char cTx)
 {
-    //Start Transmision
-    SPDR = cTx;
+	//Start Transmision by loading the char into transmit register
+	SPDR = cTx;
 
-    //Wait for transmit to finish
-    while(!(SPSR & (1 << SPIF)))
-        ;
+	//Wait for transmit to finish
+	while(!(SPSR & (1 << SPIF)))
+	;
+}
+
+
+uint8_t
+spi_rx()
+{
+    //Wait for SPI to finish reciving data
+	while(!(SPSR & (1 << SPIF)))
+		;
+	
+    //Get and return the recived data from SPDR register
+	return SPDR;
 }
--- src/node_1/lib/spi.h
+++ src/node_1/lib/spi.h
@@ -1,7 +1,9 @@
 #ifndef SPI_H
 #define SPI_H
 
-void spi_init();
-void spi_transmit(char);
+void	spi_master_init();		//Init function of SPI as master
+void	spi_slave_init();		//Init function of SPI as slave
+void	spi_tx(char);			//Transmit char over SPI
+uint8_t spi_rx();				//Read char over SPI							
 
 #endif;
