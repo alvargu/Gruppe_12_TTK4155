b1e9f58c64bc040568a70c4331c5fac560c77c66
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-11-11 21:15:04 +0100

    Update pwm_ctrl.c

--- src/node_2/pwm_ctrl.c
+++ src/node_2/pwm_ctrl.c
@@ -3,28 +3,30 @@
 void
 pwm_init()
 {
-	PIOB -> PIO_PER		|= PIO_PB15;			//Enable pin PB15 as output
-	//PWM is going to output on said pin
+		PMC -> PMC_PCER1 |= PMC_PCER1_PID36;
 	
-	//Clock Config
+	//PIOB -> PIO_PER		|= PIO_PB13;			//Enable pin PB13 as output
+												//PWM is going to output on said pin
+	PIOB -> PIO_ABSR	|= PIO_PB13;
+	PIOB -> PIO_PDR		|= PIO_PB13;
+	
+												//Clock Config
 	PWM -> PWM_CLK		|= PWM_CLK_DIVB(100);	//Enable CLK Divider B
-	//Assumed that is is used for PWMH
+												//Assumed that is is used for PWMH
 	
 	PWM -> PWM_CLK		|= PWM_CLK_PREB(5);		//Configure Pre-scaler as MCK/32
-	//Later on divide it by value of the prescaler
-	//meaning that the final freq is MCK/32/100
-	
-	//Channel Config
-	//PWM -> PWM_IER1		|= PWM_ENA_CHID3;	//Enable interrupt on channel 3
-	//Change interrupt type to IER2?
-	
-	PWM -> PWM_SCM		|= PWM_SCM_SYNC3		//Configure Channel 3 as synchronous
-	|  PWM_SCM_UPDM_MODE0;	//Manual control of update channels and update registers
-	//Now its possible to use UPD registers instead of direct registers
-	
-	default_values();							//Load default values for the servo and the rest of the system
-	
-	PWM -> PWM_ENA		|= PWM_ENA_CHID3;		//Enable PWM on channel 3
+												//Later on divide it by value of the prescaler
+												//meaning that the final freq is MCK/32/100
+														
+	PWM -> PWM_SCM		 = PWM_SCM_SYNC1		//Configure Channel 1 as synchronous
+						|  PWM_SCM_UPDM_MODE0;	//Manual control of update channels and update registers
+												//Now its possible to use UPD registers instead of direct registers
+																	
+	PWM -> PWM_CH_NUM[1].PWM_CMR = PWM_CMR_CPRE_CLKB;
+	PWM -> PWM_CH_NUM[1].PWM_CPRD = 2625;
+	PWM -> PWM_CH_NUM[1].PWM_CDTY = 1312;
+																							
+	PWM -> PWM_ENA		= PWM_ENA_CHID1;		//Enable PWM on channel 1
 }
 
 void
