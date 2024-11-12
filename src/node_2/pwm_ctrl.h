89268932a0ad85ef7c2f0838452887662c6d32ae
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-11-12 17:18:58 +0100

    Update pwm_ctrl.h

--- src/node_2/pwm_ctrl.h
+++ src/node_2/pwm_ctrl.h
@@ -1,16 +1,24 @@
 #ifndef PWM_CTRL
 #define PWM_CTRL
-
 #include "sam.h"
 
-#define PWM_MIN 90
-#define PWM_MAX 210
+#define F_CPU 84000000
+
+#define PWM_FRQ	50
+#define PWM_MS	F_CPU/512						
+#define PWM_MIN PWM_MS/PWM_FRQ/20*9/10			
+#define PWM_MAX PWM_MS/PWM_FRQ/20*21/10			
+
+//#define PWM_LEFT	PWM_MS/PWM_FRQ/20			
+//#define PWM_RIGHT	PWM_MS/PWM_FRQ/20*2			
+//#define PWM_CENTER	PWM_MS/PWM_FRQ/20*15/10		
+
+#define JOYSTICK_VAL_RANGE 200
 
 void pwm_init();
-void pwm_udc(uint16_t);
-void pwm_upv(uint16_t);
-void pwm_udt(uint16_t);
-void pwm_apply_timings();
-void default_values();
+void pwm_update_duty_cycle(uint16_t);
+void pwm_update_period_value(uint16_t);
+void pwm_update_dead_time(uint16_t);
+uint16_t pwm_scale(uint16_t);
  
 #endif
