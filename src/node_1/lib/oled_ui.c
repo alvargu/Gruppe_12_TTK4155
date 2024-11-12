85eb5d2d1ff717e79578f70024ebc9e38078f086
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-11-12 15:34:39 +0100

    Finish oled_ui framework

--- src/node_1/lib/oled_ui.c
+++ src/node_1/lib/oled_ui.c
@@ -1,5 +1,8 @@
 #include "oled_ui.h"
 
+#define F_CPU 4915200 //Clock Speed
+#include <util/delay.h>
+
 void oled_ui_draw_screen(menu_t* const menu)
 {
 		uint8_t above_item;
@@ -23,9 +26,21 @@ void oled_ui_draw_screen(menu_t* const menu)
 				below_item = active -1;
 		}
 
-
 		oled_clear();
+
+		oled_goto_pos(0,0);
+		oled_printf("   |             |   ", FONT_MEDIUM);
+		oled_goto_pos(1,0);
+		oled_printf("   |             |   ", FONT_MEDIUM);
+		oled_goto_pos(3,0);
+		oled_printf("   |             |   ", FONT_MEDIUM);
+		oled_goto_pos(5,0);
+		oled_printf("   |             |   ", FONT_MEDIUM);
+		oled_goto_pos(7,0);
+		oled_printf("   |             |   ", FONT_MEDIUM);
+
 		oled_goto_pos(2,0);
+		oled_printf("   ", FONT_MEDIUM);
 		oled_printf(((menu -> menu_item)[above_item]), FONT_MEDIUM);
 
 		oled_goto_pos(4,0);
@@ -34,6 +49,7 @@ void oled_ui_draw_screen(menu_t* const menu)
 		oled_printf(" < ", FONT_MEDIUM);
 
 		oled_goto_pos(6,0);
+		oled_printf("   ", FONT_MEDIUM);
 		oled_printf(((menu -> menu_item)[below_item]), FONT_MEDIUM);
 }
 
@@ -68,5 +84,40 @@ void oled_ui_update_active(menu_t* menu, joystick_direction_t direction)
 						//do nothing
 		}
 
+}
+
+uint8_t oled_ui_select_item(menu_t* const menu)
+{
+		//Button pressed corresponds to "logic low" on pin 4
+		if (!((PIND >> PD4) & 0x1))
+		{
+				return menu -> active_item;
+		}
+		return 199;
+}
+
+uint8_t oled_ui_handler(menu_t* menu)
+{
+
+		raw_adc_data_t adc_readout;
+		joystick_direction_t direction;
+
+		adc_sample(&adc_readout);
+		direction = joystick_get_direction(&adc_readout);
+
+		if (direction != neutral)
+		{
+				_delay_ms(300);
+				adc_sample(&adc_readout);
+				direction = joystick_get_direction(&adc_readout);
+
+				if (direction != neutral)
+				{
+						oled_ui_update_active(menu, direction);
+						oled_ui_draw_screen(menu);
+				}
+		}
+
+		return oled_ui_select_item(menu);
 }
 
--- src/node_1/lib/oled_ui.h
+++ src/node_1/lib/oled_ui.h
@@ -17,10 +17,15 @@ typedef struct{
 		char menu_item[MAX_MENU_LENGTH][MAX_CHARS];
 } menu_t;
 
+uint8_t oled_ui_handler(menu_t* menu);
+
+//Updates the active menu item based on joystick direction.
 void oled_ui_update_active(menu_t* menu, joystick_direction_t direction);
 
+//Draws screen according to menu. (active item in the middle of the screen, 
+//with the correct items above and below.
 void oled_ui_draw_screen(menu_t* const menu);
 
-uint8_t oled_ui_select_item(menu_t* const menu, /* something to detect joystick button*/);
-
+//Checks if joystick button is pressed, if it is, returns index of the current active menu item.
+uint8_t oled_ui_select_item(menu_t* const menu);
 #endif
