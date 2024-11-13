f51c2969b4ee66b4e02f277bda321ffd514613ba
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2024-11-13 12:06:23 +0100

    Add files via upload

--- src/node_2/main.c
+++ src/node_2/main.c
@@ -1,31 +1,39 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "sam.h"
+#include <stdio.h>
 
-/*
- * Remember to update the Makefile with the (relative) path to the uart.c file.
- * This starter code will not compile until the UART file has been included in the Makefile. 
- * If you get somewhat cryptic errors referencing functions such as _sbrk, 
- * _close_r, _write_r, _fstat etc, you have most likely not done that correctly.
+#include "lib/ir_driver.h"
+#include "lib/uart.h"
+#include "lib/can.h"
+#include "lib/pwm_ctrl.h"
 
- * If you get errors such as "arm-none-eabi-gcc: no such file", you may need to reinstall the arm gcc packages using
- * apt or your favorite package manager.
- */
-//#include "../path_to/uart.h"
+#define F_CPU 84000000
+#define BAUD 9600
 
-int main()
+CanInit bit_timing = {.phase2 = 5, .propag = 1, .phase1 = 6, .sjw = 0, .brp = 41, .smp = 0};
+
+int 
+main(void)
 {
     SystemInit();
+	WDT -> WDT_MR |= WDT_MR_WDDIS;
+	
+	uart_init(F_CPU,BAUD);
+	adc_init();
+	pwm_init();
+	can_init(bit_timing, 0);
+	
+	CanMsg recive_msg;
 
-    WDT->WDT_MR = WDT_MR_WDDIS; //Disable Watchdog Timer
-
-    //Uncomment after including uart above
-    //uart_init(/*cpufreq*/, /*baud*/);
-    //printf("Hello World\n\r");
-
-    while (1)
+	printf("Game Started!\n\r");
+    while(1) 
     {
-        /* code */
+		while(!(can_rx(&recive_msg)))
+		{
+			;
+		}
+		pwm_update_duty_cycle(pwm_scale(recive_msg.byte[0]));
+		if(is_goal()){
+			printf("GOAL!!!\n\r");	
+		}
     }
-    
 }
