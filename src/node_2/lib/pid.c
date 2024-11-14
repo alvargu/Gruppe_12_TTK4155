1e35d6039d5ee4d9f14d05165e8ff60420ad8cfc
Michal <31627253+Michalstank@users.noreply.github.com>
2024-11-15 00:20:39 +0100

    Update pid.c

--- src/node_2/lib/pid.c
+++ src/node_2/lib/pid.c
@@ -1,43 +1,60 @@
 #include "pid.h"
 
+//File is named pid, but the design has changed such that 
+//it is now a PI controller.
+
 // Instantiate pid struct for global values
 PID pid;
 
+uint64_t timing_var_i = 0;
+uint64_t timing_var_d = 0;
+
+
+volatile int16_t error_sum = 0;
+
 // Initialize PID controller parameters
-void pid_init(int8_t kp, int8_t ti, int8_t td) {
+void pid_init(float kp, float ti, float td) {
 	pid.kp = kp;
 	pid.ti = ti;
-	pid.td = td;
-	pid.e_factor = 0;
-	pid.integral_value = 0;
 }
 
-int8_t pid_get_u(int8_t error)
+int16_t pid_get_u(int16_t error)
 {
-	// Clamp integral influence
-	//if ((pid.integral_value < INTEGER CLAMPS) && (pid.integral_value))
+	
+	error_sum += error;
+	
+	
+	return pid.kp * error + (pid.ti * error_sum / 128);
+	
 }
 
-uint8_t
-p_part(uint8_t error)
+
+/*
+int32_t
+p_part(int16_t error)
 {
 	return (pid.kp * error);
 }
 
-uint8_t
-i_part(uint8_t error)
+int32_t
+i_part(int16_t error)
 {
-	return (pid.integral_value + error);
+	if (time_now() > timing_var_i + TIME_CONSTANT)
+	{
+		timing_var_i = time_now();
+		pid.integral_value = (int16_t)((pid.ti)*(pid.integral_value + error));
+	}
+	return pid.integral_value;
 }
 
-uint8_t 
-d_part(uint8_t error)
+int32_t 
+d_part(int16_t error)
 {
-	return (error - pid.e_factor);	
+	if (time_now() > timing_var_d + TIME_CONSTANT)
+	{
+		timing_var_d = time_now();
+		pid.e_factor = (pid.td*(error - pid.e_factor));
+	}
+	return pid.e_factor;	
 }
-
-uint16_t
-integrator(uint8_t error)
-{
-	return p_part(error) + i_part(error) + d_part(error);
-}
+*/
