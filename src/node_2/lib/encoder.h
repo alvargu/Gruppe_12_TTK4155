8459d0be366fbc19a5ac699d2e44aa7388494a56
Michal <31627253+Michalstank@users.noreply.github.com>
2024-11-15 00:20:12 +0100

    Update encoder.h

--- src/node_2/lib/encoder.h
+++ src/node_2/lib/encoder.h
@@ -4,17 +4,13 @@
 // Include libraries needed.
 #include "sam.h"
 #include <stdint.h>
+#include <stdio.h>
+#include "uart.h"
 #include "time.h"
 
-// PC26 = ChA (D4)
-// PC25 = ChB (D5)
-#define _ENCODER_A_PIN PIO_PC25
-#define _ENCODER_B_PIN PIO_PC26
-// Define max step size for the motor box enclosure (Measured experimentally +-20)
-#define _ENCODER_STEP_MAX 1385 
 
 void encoder_init(void);
-uint8_t encoder_get_pos(void);
+int32_t encoder_get_pos(void);
 void encoder_pos_rst(void);
 
 #endif
