e17419a1cf8ccee5bfb9dfa16db69b92e462edd7
michalstank <michal2stan@gmail.com>
2024-11-14 17:17:53 +0100

    basic functions

--- src/node_2/lib/pid.c
+++ src/node_2/lib/pid.c
@@ -1,21 +1,43 @@
-
 #include "pid.h"
 
 // Instantiate pid struct for global values
 PID pid;
 
-// Initialize PID controller paramaters
-void pid_init(int8_t kp, int8_t ki, int8_t kd) {
+// Initialize PID controller parameters
+void pid_init(int8_t kp, int8_t ti, int8_t td) {
 	pid.kp = kp;
-	pid.ki = ki;
-	pid.kd = kd;
+	pid.ti = ti;
+	pid.td = td;
 	pid.e_factor = 0;
 	pid.integral_value = 0;
 }
 
 int8_t pid_get_u(int8_t error)
 {
-     // Clamp integral influence
-     if ((pid.integral_value < _IN) && (pid.integral_value))
+	// Clamp integral influence
+	//if ((pid.integral_value < INTEGER CLAMPS) && (pid.integral_value))
 }
 
+uint8_t
+p_part(uint8_t error)
+{
+	return (pid.kp * error);
+}
+
+uint8_t
+i_part(uint8_t error)
+{
+	return (pid.integral_value + error);
+}
+
+uint8_t 
+d_part(uint8_t error)
+{
+	return (error - pid.e_factor);	
+}
+
+uint16_t
+integrator(uint8_t error)
+{
+	return p_part(error) + i_part(error) + d_part(error);
+}
--- src/node_2/lib/pid.h
+++ src/node_2/lib/pid.h
@@ -5,16 +5,24 @@
 #include "time.h"
 #include "encoder.h"
 
-// Define 
+#define TIMING_FACTOR 1				//Update
+
+// Define
 typedef struct {
 	int8_t kp;
-	int8_t ki;
-	int8_t kd;
+	int8_t ti;
+	int8_t td;
 	int8_t e_factor;
 	int16_t integral_value;
 } PID;
 
-void pid_init(uint8_t kp, uint8_t ki, uint8_t kd);
+void pid_init(uint8_t kp, uint8_t ti, uint8_t td);
 void pid_get_u(uint8_t error);
+
+uint8_t p_part(uint8_t);
+uint8_t i_part(uint8_t);
+uint8_t d_part(uint8_t);
+
+uint16_t integrator(uint8_t);
 
 #endif
