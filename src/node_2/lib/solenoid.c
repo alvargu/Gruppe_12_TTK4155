57fa0966e8c3d7fa5c5425af7e1c3274871f4ade
alvargu <92863607+alvargu@users.noreply.github.com>
2024-11-14 11:29:37 +0100

    syntax changes to make compile

--- src/node_2/lib/solenoid.c
+++ src/node_2/lib/solenoid.c
@@ -1,32 +1,35 @@
 
 #include "solenoid.h"
 
-#define SOLENOID_BOUNCE_DURATION msecs(100);  // 100  [ms]
+#define SOLENOID_BOUNCE_DURATION msecs(100)  // 100  [ms]
+#define _SOLENOID_PIN PIO_PC18
 
 uint64_t start_timer_solenoid = 0;
 bool bounce_state = 0;
 
 
 // Initalization of solenoid enabling use of signal pin for solenoid
-void solenoid_init() {
+void solenoid_init(void) {
      // Enable power to PIO Port C via PMC
      PMC ->PMC_PCER0 |= PMC_PCER0_PID13;
 
      // Enable PIO control
-     PIOC->PIO_PER |= PIO_PC18;
+     PIOC->PIO_PER |= _SOLENOID_PIN;
      // Set PIO to Output Enabled Mode
-	PIOC->PIO_OER |= PIO_PC18;
+	PIOC->PIO_OER |= _SOLENOID_PIN;
+
+	solenoid_off();
 }
 
 
-void solenoid_on() {
+void solenoid_on(void) {
 	// Set pin 45 LOW for on because it is driving a PNP transistor
-	PIOC->PIO_CODR |= PIO_PC18;
+	PIOC->PIO_CODR |= _SOLENOID_PIN;
 }
 
-void solenoid_off() {
+void solenoid_off(void) {
 	// Set pin 45 HIGH for off because it is driving a PNP transistor
-	PIOC->PIO_SODR |= PIO_PC18;
+	PIOC->PIO_SODR |= _SOLENOID_PIN;
 }
 
 // Used for timing of reset 
