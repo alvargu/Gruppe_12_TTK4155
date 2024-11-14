6cc186a289a905695ec572f6ee9c813c024b3a26
Michal <31627253+Michalstank@users.noreply.github.com>
2024-11-15 00:19:34 +0100

    Update motor.h

--- src/node_2/lib/motor.h
+++ src/node_2/lib/motor.h
@@ -2,23 +2,24 @@
 #define MOTOR_H
 
 #include "sam.h"
+#include "pwm_ctrl.h"
 #include <stdint.h>
 #include "time.h"
-#include "pwm_ctrl.h"
 #include <stdio.h>
 
-#define MOTOR_DIRECTION_PIN PIO_PC23
-#define MOTOR_UPDATE_INTERVAL msecs(10)
-#define MOTOR_SPEED_MIN 0
-#define MOTOR_SPEED_MAX 200
-#define MOTOR_BUFFER_MAX 105
-#define MOTOR_BUFFER_MIN 95
-#define MOTOR_DC_MAX 2050
-#define MOTOR_DC_MIN 0
+#define _MOTOR_DIRECTION_PIN PIO_PC23
+#define _MOTOR_UPDATE_INTERVAL msecs(10)
+#define _MOTOR_SPEED_MIN (-700)
+#define MOTOR_SPEED_CENTER 0
+#define _MOTOR_SPEED_MAX 700
+#define MOTOR_BUFFER_MAX 20
+#define MOTOR_BUFFER_MIN -20
+#define _MOTOR_DC_MAX 2050
+#define _MOTOR_DC_MIN 0
 #define MOTOR_CALIB_DELAY msecs(2000)
 
 void motor_init(void);
 void motor_return_to_zero(void);
-void motor_speed_set(uint16_t motor_speed);
+void motor_speed_set(int16_t motor_speed);
 
 #endif
