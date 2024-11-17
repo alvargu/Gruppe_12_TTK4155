4b6d5af4e196441dbd3a5351dce460ece16ec6e2
Michal <31627253+Michalstank@users.noreply.github.com>
2024-11-17 18:55:15 +0100

    Minor Fixes

--- src/node_2/lib/pwm_ctrl.c
+++ src/node_2/lib/pwm_ctrl.c
@@ -1,7 +1,7 @@
 #include "pwm_ctrl.h"
 
 void
-pwm_init()
+pwm_init(void)
 {
 	PMC -> PMC_PCER1	|= PMC_PCER1_PID36;				//Enable Peripheral in PMC
 	
@@ -18,7 +18,7 @@ pwm_init()
 	PWM -> PWM_CH_NUM[1].PWM_CMR	|= PWM_CMR_CPRE_CLKB			//Connect Channel 1 to CLK B
 					|  PWM_CMR_CPOL;			//Invert PWM output meaning duty cycle is high
 	PWM -> PWM_CH_NUM[1].PWM_CPRD = PWM_MS/PWM_FRQ;				//Asign Period Value (~3_280)
-	PWM -> PWM_CH_NUM[1].PWM_CDTY = PWM_MIN;				//Centers the servo
+	PWM -> PWM_CH_NUM[1].PWM_CDTY = PWM_MS/PWM_FRQ/2;				//Centers the servo
 																							
 	PWM -> PWM_ENA		= PWM_ENA_CHID1;				//Enable PWM on channel 1
 }
@@ -39,7 +39,8 @@ pwm_update_duty_cycle(uint16_t dc_val)
 		PWM -> PWM_CH_NUM[1].PWM_CDTYUPD		= PWM_MIN;
 			
 	}
-	PWM -> PWM_SCUC					       |= PWM_SCUC_UPDULOCK;	//Main "command" used to load the update registers to main PWM registers
+	//Main "command" used to load the update registers to main PWM registers
+	PWM -> PWM_SCUC					       |= PWM_SCUC_UPDULOCK;
 }
 
 void
@@ -62,5 +63,45 @@ uint16_t
 pwm_scale(uint16_t x_val)
 {
 	uint16_t max_val = PWM_MAX - PWM_MIN;
-	return JOYSTICK_VAL_RANGE - (x_val*max_val/JOYSTICK_VAL_RANGE) + PWM_MIN;
+	return (x_val*max_val/JOYSTICK_VAL_RANGE) + PWM_MIN;
+}
+
+void
+pwm_motor_init(void)
+{
+	PMC -> PMC_PCER1				|= PMC_PCER1_PID36;		//Enable Peripheral in PMC
+	
+	PIOB -> PIO_ABSR				|= PIO_PB12;			//Set Pin PB12 to be controlled by PWM
+	PIOB -> PIO_PDR					|= PIO_PB12;			//Disable control of the pin by PIO
+	
+	PWM -> PWM_CLK					|= PWM_CLK_DIVA(1);		//Enable CLK Divider A
+	PWM -> PWM_CLK					|= PWM_CLK_PREA(0b1010);//Configure DivA as MCK/1024
+	
+	PWM -> PWM_SCM					|= PWM_SCM_SYNC0		//Configure Channel 0 as non synchronous
+									|  PWM_SCM_UPDM_MODE0;	//Manual control of update channels and update registers
+						
+	PWM -> PWM_CH_NUM[0].PWM_CMR	|= PWM_CMR_CPRE_CLKA	//Connect Channel 0 to CLK A
+									|  PWM_CMR_CPOL;		//Invert PWM output meaning duty cycle is high
+									
+	PWM -> PWM_CH_NUM[0].PWM_CPRD	 = PWM_MOTOR_PER;		//Assign Period Value
+	PWM -> PWM_CH_NUM[0].PWM_CDTY	 = 0;					//0% Duty Cycle for init
+	
+	PWM -> PWM_ENA					 = PWM_ENA_CHID0;		//Enable PWM on channel 0
+}
+
+void
+pwm_set_motor_speed(uint16_t dc_val)
+{
+	if(dc_val >= 0 && dc_val <= PWM_MOTOR_PER)
+	{
+		PWM -> PWM_CH_NUM[0].PWM_CDTY = dc_val;
+	}
+	else if(dc_val > PWM_MOTOR_PER)
+	{
+		PWM -> PWM_CH_NUM[0].PWM_CDTY = PWM_MOTOR_PER;
+	}
+	else if(dc_val < 0)
+	{
+		PWM -> PWM_CH_NUM[0].PWM_CDTY = 0;
+	}
 }
--- src/node_2/lib/pwm_ctrl.h
+++ src/node_2/lib/pwm_ctrl.h
@@ -9,16 +9,23 @@
 #define PWM_MIN PWM_MS/PWM_FRQ/20*9/10			
 #define PWM_MAX PWM_MS/PWM_FRQ/20*21/10			
 
+#define PWM_MOTOR_PER F_CPU/1024/40
+
 //#define PWM_LEFT	PWM_MS/PWM_FRQ/20			
 //#define PWM_RIGHT	PWM_MS/PWM_FRQ/20*2			
 //#define PWM_CENTER	PWM_MS/PWM_FRQ/20*15/10		
 
 #define JOYSTICK_VAL_RANGE 200
 
-void pwm_init();
+void tc_init(void);
+
+void pwm_init(void);
 void pwm_update_duty_cycle(uint16_t);
 void pwm_update_period_value(uint16_t);
 void pwm_update_dead_time(uint16_t);
 uint16_t pwm_scale(uint16_t);
+ 
+void pwm_motor_init(void);
+void pwm_set_motor_speed(uint16_t); 
  
 #endif
